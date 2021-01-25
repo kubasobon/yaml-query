eb6e697f6b87d2fdfc4478a636bb25d3a7f4221c
kubasobon <wtty.fool@gmail.com>
2021-01-25 09:23:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="yaml-query",
-    version="0.0.1",
+    version="0.0.2",
     author="kubasobon",
     author_email="wtty.fool@gmail.com",
     description="yaml query library",
